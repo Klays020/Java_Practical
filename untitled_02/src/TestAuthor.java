a7bdafaef6ae5c3996a6f57584ba13641bc0ece4
Klays020 <113058901+Klays020@users.noreply.github.com>
2022-12-22 20:53:18 +0300

    Add files via upload

---
+++ untitled_02/src/TestAuthor.java
@@ -0,0 +1,6 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author b1 = new Author("Yegor Kireev", "Kireev@somewhere.com", 'm');
+        System.out.println(b1);
+    }
+}
